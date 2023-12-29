e240a619e973aacb4c5bf60fe9e252aeea79df64
JOSHI HARSH <124500473+HarshJoshiKutch@users.noreply.github.com>
2023-12-29 08:04:27 +0530

    Add files via upload

---
+++ GET_DATA.php
@@ -0,0 +1,11 @@
+<?php
+// have je data malse e array store 
+echo "<pre>";
+print_r($_GET);
+echo "</pre>";
+
+echo $_GET["fname"]; 
+// echo $_POST["fname"]; jo post method no use thayo hoi to
+
+
+?>
